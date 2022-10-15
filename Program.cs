28c7825e755f3cebaa281fe43014c33f4bc5769c
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-15 18:46:42 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day10AddressBookSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System");
+        }
+    }
+}
